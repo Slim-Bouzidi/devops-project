a00cd4a2f2764f56eba6e64fb97e3eff0aa6d26e
Sliim Bouzidi <sliimbouzidi@gmail.com>
2025-12-13 17:40:39 +0100

    adding age field

--- src/main/java/com/slimbouzidi/devopsproject/entity/User.java
+++ src/main/java/com/slimbouzidi/devopsproject/entity/User.java
@@ -19,6 +19,8 @@ public class User {
 
     private String name;
 
+    private int age;
+
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Product> products;
 }
